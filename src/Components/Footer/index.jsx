e03cf13a36c5bfd89bc3e1c7c1cd68065370ce76
sbellotlecoq <sbellotlecoq@gmail.com>
2023-06-19 17:43:54 +0200

    page home en cours

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import { Facebook, Instagram, Twitter } from "@mui/icons-material";
+import { Box } from "@mui/material";
+
+import "/home/sbellotlecoq/THP/final/PawsDetente/src/App.css";
+
+export default function Footer() {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        backgroundColor: "#1976d2",
+        p: 6,
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container spacing={5}>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="white" gutterBottom>
+              A propos
+            </Typography>
+            <Typography variant="body2" color="white">
+              Nous sommes l'entreprise Paws Détente, pourvoyeur de bonne humeur pour vos amis les plus fidèles.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="white" gutterBottom>
+              Nous contacter
+            </Typography>
+            <Typography variant="body2" color="white">
+              Rue des animaux
+            </Typography>
+            <Typography variant="body2" color="white">
+              Email: pawsdétente@yopmail.com
+            </Typography>
+            <Typography variant="body2" color="white">
+              Phone: +1 234 567 8901
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="white" gutterBottom>
+              Follow Us
+            </Typography>
+            <Link href="https://www.facebook.com/" color="inherit">
+              <Facebook />
+            </Link>
+            <Link
+              href="https://www.instagram.com/"
+              color="inherit"
+              sx={{ pl: 1, pr: 1 }}
+            >
+              <Instagram />
+            </Link>
+            <Link href="https://www.twitter.com/" color="inherit">
+              <Twitter />
+            </Link>
+          </Grid>
+        </Grid>
+        
+      </Container>
+    </Box>
+  );
+}
