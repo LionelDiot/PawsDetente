e4efb11fe84b70941505518d1667307c016c9620
sbellotlecoq <sbellotlecoq@gmail.com>
2023-06-21 16:26:18 +0200

    search ok centré

--- src/Components/Users/register.jsx
+++ src/Components/Users/register.jsx
@@ -44,13 +44,16 @@ export default function SignUp() {
         };
 
         try {
-            const response = await fetch("http://127.0.0.1:3000/users", {
+            const response = await fetch(
+              "https://api-paws-detente-6e0fafb6dbaa.herokuapp.com/users",
+              {
                 method: "POST",
                 headers: {
-                    "Content-Type": "application/json",
+                  "Content-Type": "application/json",
                 },
                 body: JSON.stringify(data),
-            });
+              }
+            );
 
             if (response.ok) {
                 const responseData = await response.json();
