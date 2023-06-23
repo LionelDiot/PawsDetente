969cff8e57da43a12cb3afa73e2505e0234243db
sbellotlecoq <sbellotlecoq@gmail.com>
2023-06-23 12:38:31 +0200

    index mieux avec texte et logo sur jumbotron

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -85,7 +85,7 @@ function Navbar() {
               textDecoration: "none",
             }}
           >
-            HOME
+            PAWS DETENTE
           </Typography>
 
           {isMobileScreen && (
