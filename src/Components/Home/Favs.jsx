6a26dada1710b47c62ae2cb112002f9312346d7a
sbellotlecoq <sbellotlecoq@gmail.com>
2023-06-29 23:21:07 +0200

    responsive favs et titre home pour cards

--- src/Components/Home/Favs.jsx
+++ src/Components/Home/Favs.jsx
@@ -2,76 +2,67 @@ import React, { useState, useEffect } from "react";
 import { useAtomValue } from "jotai";
 import { currentUserAtom } from "../../Atoms/currentuser";
 import { loggedInAtom } from "../../Atoms/loggedin";
-import {
-    Typography,
-} from "@mui/material";
-import CardItem from "../Style/Card"; 
+import { Typography } from "@mui/material";
+import CardItem from "../Style/Card";
 import { Grid, Box } from "@mui/material";
 import Container from "@mui/material/Container";
 
 export default function MyProfile() {
-    const [monprofil, setMonprofil] = useState("");
-    const [responseData, setResponseData] = useState(null);
-    const [userData, setUserData] = useState(null);
-    
-    const [favorites, setFavorites] = useState(null);
-    const user = useAtomValue(currentUserAtom);
-    const loggedIn = useAtomValue(loggedInAtom);
+  const [monprofil, setMonprofil] = useState("");
+  const [responseData, setResponseData] = useState(null);
+  const [userData, setUserData] = useState(null);
 
-    useEffect(() => {
-        if (loggedIn) {
-            fetch("https://api-paws-detente-6e0fafb6dbaa.herokuapp.com/favorites", {
-                method: "get",
-                headers: {
-                    Authorization: `${user}`,
-                    "Content-Type": "application/json",
-                },
-            })
-                .then((response) => response.json()) // Handle the response properly
-                .then((responseData) => {
-                    setFavorites(responseData); // Separate state for favorites data
-                    console.log(responseData);
-                })
-                .catch((error) => {
-                    console.error("Error:", error);
-                    setMonprofil(
-                        "Une erreur s'est produite lors de la récupération des données."
-                    );
-                });
-        } else {
-            setMonprofil(
-                "Vous n'êtes pas connecté. Vous n'avez donc pas de profil."
-            );
-        }
-    }, [user, loggedIn]);
+  const [favorites, setFavorites] = useState(null);
+  const user = useAtomValue(currentUserAtom);
+  const loggedIn = useAtomValue(loggedInAtom);
 
-    return (
-      <Container
-        sx={{
-          width: "1200px",
-          py: 4,
-          display: "flex",
-          flexDirection: "column",
-        }}
-        maxWidth="lg"
-      >
-        <Box textAlign="center">
-          <Typography variant="h4" marginBottom={4}>
-            Bienvenue sur la page des favoris
-          </Typography>
+  useEffect(() => {
+    if (loggedIn) {
+      fetch("https://api-paws-detente-6e0fafb6dbaa.herokuapp.com/favorites", {
+        method: "get",
+        headers: {
+          Authorization: `${user}`,
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json()) // Handle the response properly
+        .then((responseData) => {
+          setFavorites(responseData); // Separate state for favorites data
+          console.log(responseData);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          setMonprofil(
+            "Une erreur s'est produite lors de la récupération des données."
+          );
+        });
+    } else {
+      setMonprofil("Vous n'êtes pas connecté. Vous n'avez donc pas de profil.");
+    }
+  }, [user, loggedIn]);
 
-          <Grid container spacing={2} columns={{ xs: 4, sm: 8, md: 12 }}>
-            {favorites ? (
-              favorites.map((item) => (
-                <Grid item xs={2} sm={4} md={3} key={item.id}>
+  return (
+    <Box textAlign="center" marginTop={4}>
+      <Typography variant="h4">Bienvenue sur la page des favoris</Typography>
+      <Container sx={{ py: 4 }}>
+        <Grid container spacing={0} sx={{ justifyContent: "center" }}>
+          {favorites ? (
+            favorites.map((item) => (
+              <Grid item key={item} xs={12} sm={6} md={4} lg={3}>
+                <Box display="flex" justifyContent="center">
                   <CardItem item={item} />
-                </Grid>
-              ))
-            ) : (
-              <p>Votre liste de favoris est vide.</p>
-            )}
-          </Grid>
-        </Box>
+                </Box>
+              </Grid>
+            ))
+          ) : (
+            <Grid item xs={12}>
+              <Typography variant="body1">
+                Votre liste de favoris est vide.
+              </Typography>
+            </Grid>
+          )}
+        </Grid>
       </Container>
-    );
+    </Box>
+  );
 }
