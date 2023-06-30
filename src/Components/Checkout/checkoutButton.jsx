b32ae0af8e4436871c2349430f8cd98a4746322e
Djo-75 <j.pecquet@live.fr>
2023-06-30 14:28:07 +0200

    bouton que je n'utilise finalement pas

--- src/Components/Checkout/checkoutButton.jsx
+++ src/Components/Checkout/checkoutButton.jsx
@@ -9,35 +9,37 @@ export default function CheckoutButton({ total }) {
   const loggedIn = useAtomValue(loggedInAtom);
 
   const handlePayment = async () => {
-    if (loggedIn) { try {
-      // Make the API request to create a checkout session
-      const response = await fetch('https://api-paws-detente-6e0fafb6dbaa.herokuapp.com/checkout', {
-        method: 'GET',
-        headers: {
-          "Authorization": `${user}`,
-          'Content-Type': 'application/json',
+    if (loggedIn) {
+      try {
+        // Make the API request to create a checkout session
+        const response = await fetch('https://api-paws-detente-6e0fafb6dbaa.herokuapp.com/checkout', {
+          method: 'GET',
+          headers: {
+            "Authorization": `${user}`,
+            'Content-Type': 'application/json',
+          }
+        });
+
+        // Handle the API response
+        if (response.ok) {
+          const { sessionId } = await response.json();
+
+          // Load the Stripe instance
+          const stripe = await loadStripe('pk_test_51MeC2IGFwxWB3B7MgBO0JGpTwYeudavjvvMa9Jbcu4PN1gerYAI9ErBoETZPAKKbF21EHxu8MUfATXaTvSrZgYSj00YrK3BnKT');
+
+          // Redirect the user to the Stripe checkout page
+          stripe.redirectToCheckout({ sessionId });
+        } else {
+          console.error('Error:', response.status);
         }
-      });
-
-      // Handle the API response
-      if (response.ok) {
-        const { sessionId } = await response.json();
-
-        // Load the Stripe instance
-        const stripe = await loadStripe('pk_test_51MeC2IGFwxWB3B7MgBO0JGpTwYeudavjvvMa9Jbcu4PN1gerYAI9ErBoETZPAKKbF21EHxu8MUfATXaTvSrZgYSj00YrK3BnKT');
-
-        // Redirect the user to the Stripe checkout page
-        stripe.redirectToCheckout({ sessionId });
-      } else {
-        console.error('Error:', response.status);
+      } catch (error) {
+        console.error('Error:', error);
       }
-    } catch (error) {
-      console.error('Error:', error);
-    }}
+    }
   };
 
   return (
-    <Button variant="outlined" onClick={handlePayment} color="inherit">
+    <Button variant="contained" onClick={handlePayment} >
       Valider la commande
     </Button>
   );
